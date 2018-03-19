55ca32c4f605d6a1561503c64097a8adfba7d330
UjjawalX <besrau@yahoo.com>
2018-03-20 04:51:55 +0530

    first commit for mockMvcTest

---
+++ mockMvcTest/src/main/java/com/ujjawal/springtest/mockMvcTest/MockMvcTestApplication.java
@@ -0,0 +1,12 @@
+package com.ujjawal.springtest.mockMvcTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMvcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMvcTestApplication.class, args);
+	}
+}
