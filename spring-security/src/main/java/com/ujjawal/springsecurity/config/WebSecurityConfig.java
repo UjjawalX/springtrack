c6ff59d1d80936ec8b9dca0d0773c7dce2edb8fc
ujjawal <besrau@yahoo.com>
2019-12-08 19:45:40 +0530

    modified the httSecurity configuration to admin

--- spring-security/src/main/java/com/ujjawal/springsecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/ujjawal/springsecurity/config/WebSecurityConfig.java
@@ -2,30 +2,28 @@ package com.ujjawal.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        /*http
                 .authorizeRequests()
                     .antMatchers("/", "/home").permitAll().anyRequest().authenticated().and()
                 .formLogin()
                     .loginPage("/login").permitAll().and()
                 .logout()
-                    .permitAll();
+                    .permitAll();*/
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").hasRole("USER")
+                .and().formLogin();
     }
 
     /*@Bean
@@ -37,7 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("raj").password(passwordEncoder().encode("ji")).roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER").and()
+                .withUser("admin").password((passwordEncoder().encode("password"))).roles("ADMIN", "USER");
     }
 
     @Bean
